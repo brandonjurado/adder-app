7e33dea1d850cf70e19eded54a0330ab61779b7c
Brandon Jurado <brandonjurado13@yahoo.com>
2021-07-14 13:47:48 -0500

    Add fault tolerance to controller endpoint

--- src/main/java/com/brandon/bootcamp/controller/SumController.java
+++ src/main/java/com/brandon/bootcamp/controller/SumController.java
@@ -2,6 +2,7 @@ package com.brandon.bootcamp.controller;
 
 import com.brandon.bootcamp.model.Sum;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SumController {
 
-    @GetMapping(value = "/{num1}/{num2}")
-    public Sum getSum(@PathVariable String num1, @PathVariable String num2) {
+    @GetMapping(value = {"/", "/{num1}", "/{num1}/{num2}"})
+    public ResponseEntity getSum(@PathVariable(required = false) String num1, @PathVariable(required = false) String num2) {
         int n1 = 0, n2 = 0;
         try {
-            n1 = Integer.parseInt(num1);
-            n2 = Integer.parseInt(num2);
+            if (num1 != null) {
+                n1 = Integer.parseInt(num1);
+            } else {
+                return ResponseEntity.badRequest().body("Hello, world! Please provide two integer values to return a sum (e.g. /10/5)");
+            }
+            if (num2 != null) {
+                n2 = Integer.parseInt(num2);
+            }
         } catch (NumberFormatException e) {
-            log.error("Please enter an integer to calculate sum.", e);
+            log.error("ERROR: Invalid input provided by user.", e);
+            return ResponseEntity.badRequest().body("Please enter a valid integer to calculate sum (e.g. /10/5).");
         }
-        return new Sum(Integer.sum(n1, n2));
+        return ResponseEntity.ok(new Sum(Integer.sum(n1, n2)));
     }
 }
