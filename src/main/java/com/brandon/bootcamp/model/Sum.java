d0fc6efedc7a4d1dbfaffa7743e5182bb7ff8e56
Brandon Jurado <brandonjurado13@yahoo.com>
2021-07-14 13:48:28 -0500

    Use Lombok

--- src/main/java/com/brandon/bootcamp/model/Sum.java
+++ src/main/java/com/brandon/bootcamp/model/Sum.java
@@ -1,9 +1,10 @@
 package com.brandon.bootcamp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+@Value
+@AllArgsConstructor
 public class Sum {
     public int sum;
-
-    public Sum(int sum) {
-        this.sum = sum;
-    }
 }
